f11324feadebdbd61af875170310f12559006565
Paddy Foran <paddy@iron.io>
2013-09-09 21:45:00 -0400

    Fix tab spacing on output of apidef-test.
    By using the text/tabwriter package, all the tabstops when printing
    output from apidef-test.go now line up into neat columns, regardless of
    content length.

--- apidef-test.go
+++ apidef-test.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"github.com/iron-io/jarvis/apidef"
+	"os"
+	"text/tabwriter"
 )
 
 func main() {
@@ -10,14 +12,17 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	w := new(tabwriter.Writer)
+	w.Init(os.Stdout, 0, 8, 0, '\t', 0)
 	for id, resource := range resources {
-    endpoints := resource.BuildEndpoints()
-    if len(endpoints) < 1 {
-      continue
-    }
-		fmt.Printf("\n%s (%s)\n", resource.Name, id)
-    for _, endpoint := range endpoints {
-      fmt.Printf("\t%s /%s\t%s\n", endpoint.Verb, endpoint.Path, endpoint.Description)
-    }
+		endpoints := resource.BuildEndpoints()
+		if len(endpoints) < 1 {
+			continue
+		}
+		fmt.Fprintf(w, "\n%s (%s)\n", resource.Name, id)
+		for _, endpoint := range endpoints {
+			fmt.Fprintf(w, "\t%s /%s\t%s\n", endpoint.Verb, endpoint.Path, endpoint.Description)
+		}
 	}
+	w.Flush()
 }
